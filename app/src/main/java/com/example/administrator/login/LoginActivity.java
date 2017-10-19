ef2d4af70f9df8687d7a8b420904cd6c550f7934
1560311101 <1902139457@qq.com>
2017-10-19 11:38:06 +0800

    turn yemian

--- app/src/main/java/com/example/administrator/login/LoginActivity.java
+++ app/src/main/java/com/example/administrator/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.administrator.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Administrator on 2017/10/16.
@@ -14,5 +17,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = (Button)findViewById(R.id.Login_TextView_Send);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
